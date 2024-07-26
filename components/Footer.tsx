7035ab199422574eefcfc03657bbffe426e0f397
Ben <52263490+kn4ihp@users.noreply.github.com>
2024-07-26 00:02:36 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="flex flex-col  text-black-100 mt-5 border-t border-gray-100">
       <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
-          <Image src="/logo.png" alt="logo" width={118} height={18} className="object-contain" />
+          <Image src="./logo.png" alt="logo" width={118} height={18} className="object-contain" />
           <p className="text-base text-gray700">© 2024 <br/> All rights reserved.</p>
         </div>
         {/* 3 columns with 3 links under each */}
