1fa7e815e3f3e5132887676963bae6cd8b4dd42b
Ben <52263490+kn4ihp@users.noreply.github.com>
2024-07-26 16:11:03 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
         </div>
         <div className="flex flex-col justify-start items-start gap-1 px-6">
           <h3 className="text-lg font-bold text-gray-800">LINKS</h3>
-          <Link href="/scholarships" className="text-base text-gray-700">Scholarships</Link>
+          <Link href="https://educationusa.state.gov" className="text-base text-gray-700">Scholarships Opportunities</Link>
           <Link href="https://arxiv.org/abs/2310.20488" text-base text-gray-700">Papers</Link>
           <Link href="/highlights" className="text-base text-gray-700">Student Higlights</Link>
         </div>
