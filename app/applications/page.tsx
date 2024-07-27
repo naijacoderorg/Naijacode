dad3aa721766e28c768eeb8c9e7a30e64e78fd48
Ben <52263490+kn4ihp@users.noreply.github.com>
2024-07-27 15:23:00 -0400

    Update page.tsx

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -6,7 +6,7 @@ const ProgramOverview = () => {
     <div className="flex xl:flex-col flex-col gap-5 relative z-0 max-w-[1440px] mx-auto p-36">
       <div className="hero__title ">
         <h1 className="hero__title justify-center text-center items-center">
-          This will link to google docs
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSeFyHnAmFwI7QUpAiSD3PaUY-8dPyDl_SuXG9Nt0j6SpILIUw/closedform>his will link to google docs</a>
         </h1>
         <h1 className="text-center hero__subtitle font-bold">
          Applications page
