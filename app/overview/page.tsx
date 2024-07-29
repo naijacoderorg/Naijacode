fec0eff26cfdff8ca56538093b94b3aab004a765
Ben <52263490+kn4ihp@users.noreply.github.com>
2024-07-29 15:16:51 -0400

    Update page.tsx

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -3,41 +3,24 @@ import Image from 'next/image';
 
 const ProgramOverview = () => {
   return (
-    <div className="flex xl:flex-col flex-col gap-5 relative z-0 max-w-[1440px] mx-auto p-36">
-      <div className="hero__title ">
-        <h1 className="hero__title justify-center text-center items-center">
-          Program Overview
-        </h1>
-        <h1 className="text-center hero__subtitle font-bold">
-          The NaijaCoder program is a 10-day course that teaches the basics of algorithms and computer programming.
-          The course is designed to be accessible to all students, especially those who are underprivileged.
-        </h1>
-      </div>
-
-<div className="hero__image-container">
-      <div className="hero__image">
-          <Image src="/picture_overview.png" alt="hero" fill className="object-contain" />
-      </div>
-      </div>
-
-
-
-    <div className="hero__subtitle text-center">
-        <ol>
-          <li>Day 1: Introductions and Motivations</li>
-          <li>Day 2: Types in Python</li>
-          <li>Day 3: For Loops and Recursion</li>
-          <li>Day 4: More Methods of Iteration in Python</li>
-          <li>Day 5: Objects, Libraries, Data Science</li>
-          <li>Day 6: Growth of Functions</li>
-          <li>Day 7: Searching Algorithms: Linear, Binary</li>
-          <li>Day 8: Sorting Algorithms: Bubble, Selection, Insertion</li>
-          <li>Day 9: Sorting Algorithms: Part 2</li>
-          <li>Day 10: Review, Recap, Exam</li>
-        </ol>
-    </div>
-  </div>
-
+<div class="flex xl:flex-col flex-col gap-5 relative z-0 max-w-[1440px] mx-auto p-36"><div class="hero__title "><h1 class="hero__title justify-center text-center items-center">Program Overview</h1><h1 class="text-center hero__subtitle font-bold">The NaijaCoder program is a 10-day course that teaches the basics of algorithms and computer programming. The course is designed to be accessible to all students, especially those who are underprivileged.</h1></div><div class="hero__image-container"><div class="hero__image"><img alt="hero" loading="lazy" decoding="async" data-nimg="fill" class="object-contain" src="./picture_overview.png" style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"></div></div><div class="hero__subtitle text-center"><ol><li>Day 1: Introductions and Motivations</li><li>Day 2: Types in Python</li><li>Day 3: For Loops and Recursion</li><li>Day 4: More Methods of Iteration in Python</li><li>Day 5: Objects, Libraries, Data Science</li><li>Day 6: Growth of Functions</li><li>Day 7: Searching Algorithms: Linear, Binary</li><li>Day 8: Sorting Algorithms: Bubble, Selection, Insertion</li><li>Day 9: Sorting Algorithms: Part 2</li><li>Day 10: Review, Recap, Exam</li></ol></div></div>
+<div class="hero__title "><h1 class="hero__title justify-center text-center items-center">Program Overview</h1><h1 class="text-center hero__subtitle font-bold">The NaijaCoder program is a 10-day course that teaches the basics of algorithms and computer programming. The course is designed to be accessible to all students, especially those who are underprivileged.</h1></div>
+<div class="hero__image-container"><div class="hero__image"><img alt="hero" loading="lazy" decoding="async" data-nimg="fill" class="object-contain" src="./picture_overview.png" style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"></div></div>
+<div class="hero__subtitle text-center"><ol><li>Day 1: Introductions and Motivations</li><li>Day 2: Types in Python</li><li>Day 3: For Loops and Recursion</li><li>Day 4: More Methods of Iteration in Python</li><li>Day 5: Objects, Libraries, Data Science</li><li>Day 6: Growth of Functions</li><li>Day 7: Searching Algorithms: Linear, Binary</li><li>Day 8: Sorting Algorithms: Bubble, Selection, Insertion</li><li>Day 9: Sorting Algorithms: Part 2</li><li>Day 10: Review, Recap, Exam</li></ol></div>
+<ol><li>Day 1: Introductions and Motivations</li><li>Day 2: Types in Python</li><li>Day 3: For Loops and Recursion</li><li>Day 4: More Methods of Iteration in Python</li><li>Day 5: Objects, Libraries, Data Science</li><li>Day 6: Growth of Functions</li><li>Day 7: Searching Algorithms: Linear, Binary</li><li>Day 8: Sorting Algorithms: Bubble, Selection, Insertion</li><li>Day 9: Sorting Algorithms: Part 2</li><li>Day 10: Review, Recap, Exam</li></ol>
+<li>Day 1: Introductions and Motivations</li>
+<li>Day 2: Types in Python</li>
+<li>Day 3: For Loops and Recursion</li>
+<li>Day 4: More Methods of Iteration in Python</li>
+<li>Day 5: Objects, Libraries, Data Science</li>
+<li>Day 6: Growth of Functions</li>
+<li>Day 7: Searching Algorithms: Linear, Binary</li>
+<li>Day 8: Sorting Algorithms: Bubble, Selection, Insertion</li>
+<li>Day 9: Sorting Algorithms: Part 2</li>
+<li>Day 10: Review, Recap, Exam</li>
+<ol><li>Day 1: Introductions and Motivations</li><li>Day 2: Types in Python</li><li>Day 3: For Loops and Recursion</li><li>Day 4: More Methods of Iteration in Python</li><li>Day 5: Objects, Libraries, Data Science</li><li>Day 6: Growth of Functions</li><li>Day 7: Searching Algorithms: Linear, Binary</li><li>Day 8: Sorting Algorithms: Bubble, Selection, Insertion</li><li>Day 9: Sorting Algorithms: Part 2</li><li>Day 10: Review, Recap, Exam</li></ol>
+<div class="hero__subtitle text-center"><ol><li>Day 1: Introductions and Motivations</li><li>Day 2: Types in Python</li><li>Day 3: For Loops and Recursion</li><li>Day 4: More Methods of Iteration in Python</li><li>Day 5: Objects, Libraries, Data Science</li><li>Day 6: Growth of Functions</li><li>Day 7: Searching Algorithms: Linear, Binary</li><li>Day 8: Sorting Algorithms: Bubble, Selection, Insertion</li><li>Day 9: Sorting Algorithms: Part 2</li><li>Day 10: Review, Recap, Exam</li></ol></div>
+<div class="flex xl:flex-col flex-col gap-5 relative z-0 max-w-[1440px] mx-auto p-36"><div class="hero__title "><h1 class="hero__title justify-center text-center items-center">Program Overview</h1><h1 class="text-center hero__subtitle font-bold">The NaijaCoder program is a 10-day course that teaches the basics of algorithms and computer programming. The course is designed to be accessible to all students, especially those who are underprivileged.</h1></div><div class="hero__image-container"><div class="hero__image"><img alt="hero" loading="lazy" decoding="async" data-nimg="fill" class="object-contain" src="./picture_overview.png" style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"></div></div><div class="hero__subtitle text-center"><ol><li>Day 1: Introductions and Motivations</li><li>Day 2: Types in Python</li><li>Day 3: For Loops and Recursion</li><li>Day 4: More Methods of Iteration in Python</li><li>Day 5: Objects, Libraries, Data Science</li><li>Day 6: Growth of Functions</li><li>Day 7: Searching Algorithms: Linear, Binary</li><li>Day 8: Sorting Algorithms: Bubble, Selection, Insertion</li><li>Day 9: Sorting Algorithms: Part 2</li><li>Day 10: Review, Recap, Exam</li></ol></div></div>
 
     
   );
