9ec4fea979d4d9a911bae7f194146a88b24c1ef4
Ben <52263490+kn4ihp@users.noreply.github.com>
2024-07-29 15:13:17 -0400

    Update page.tsx

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -6,24 +6,21 @@ const ProgramOverview = () => {
     <div className="flex xl:flex-col flex-col gap-5 relative z-0 max-w-[1440px] mx-auto p-36">
       <div className="hero__title ">
         <h1 className="hero__title justify-center text-center items-center">
-          Summer of 2024 Program Overview
+          Program Overview
         </h1>
         <h1 className="text-center hero__subtitle font-bold">
           The NaijaCoder program is a 10-day course that teaches the basics of algorithms and computer programming.
           The course is designed to be accessible to all students, especially those who are underprivileged.
         </h1>
-        <h2 className="font-large text-3xl>The Lagos camp is in collaboration with the AirLab of the University of Lagos. 
-          The camp will take place from the 19th of August to the 30th of August at the AI & Robotics Labs in the University of Lagos. 
-          The Abuja camp will take place at the Olumawu School in Abuja from the 12th of August to the 23rd of August.
-        </h2>
       </div>
-        
 
-<div className= "hero__image-container" >
+<div className="hero__image-container">
       <div className="hero__image">
-          <Image src="./picture_overview.png" alt="hero" fill className="object-contain" />
+          <Image src="/picture_overview.png" alt="hero" fill className="object-contain" />
       </div>
       </div>
+
+
 
     <div className="hero__subtitle text-center">
         <ol>
@@ -39,16 +36,11 @@ const ProgramOverview = () => {
           <li>Day 10: Review, Recap, Exam</li>
         </ol>
     </div>
-
-   
-      
   </div>
+
 
     
   );
 }
 
 export default ProgramOverview;
-
-
-
