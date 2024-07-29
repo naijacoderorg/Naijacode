55f6a503287513770dba74da88e386bac8f83b53
Ben <52263490+kn4ihp@users.noreply.github.com>
2024-07-29 14:44:51 -0400

    Update page.tsx

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const ProgramOverview = () => {
   return (
     <div className="flex xl:flex-col flex-col gap-5 relative z-0 max-w-[1440px] mx-auto p-36">
-      <div className="hero__title ">
+      <div className="hero__title">
         <h1 className="hero__title justify-center text-center items-center">
           Summer of 2024 Program Overview
         </h1>
@@ -12,19 +12,20 @@ const ProgramOverview = () => {
           The NaijaCoder program is a 10-day course that teaches the basics of algorithms and computer programming.
           The course is designed to be accessible to all students, especially those who are underprivileged.
         </h1>
-        <h2 className="font-large text-3xl>The Lagos camp is in collaboration with the AirLab of the University of Lagos. 
+        <h2 className="font-large text-3xl">
+          The Lagos camp is in collaboration with the AirLab of the University of Lagos. 
           The camp will take place from the 19th of August to the 30th of August at the AI & Robotics Labs in the University of Lagos. 
           The Abuja camp will take place at the Olumawu School in Abuja from the 12th of August to the 23rd of August.
         </h2>
       </div>
 
-<div className="hero__image-container">
-      <div className="hero__image">
-          <Image src="./picture_overview.png" alt="hero" fill className="object-contain" />
-      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/picture_overview.png" alt="hero" fill className="object-contain" />
+        </div>
       </div>
 
-    <div className="hero__subtitle text-center">
+      <div className="hero__subtitle text-center">
         <ol>
           <li>Day 1: Introductions and Motivations</li>
           <li>Day 2: Types in Python</li>
@@ -37,9 +38,9 @@ const ProgramOverview = () => {
           <li>Day 9: Sorting Algorithms: Part 2</li>
           <li>Day 10: Review, Recap, Exam</li>
         </ol>
-    </div>
+      </div>
 
-      <div className="hero__title ">
+      <div className="hero__title">
         <h1 className="hero__title justify-center text-center items-center">
           Summer of 2023 Highlights
         </h1>
@@ -49,7 +50,7 @@ const ProgramOverview = () => {
         </h1>
       </div>
 
-      <div className="hero__title ">
+      <div className="hero__title">
         <h1 className="hero__title justify-center text-center items-center">
           Summer of 2022 Highlights
         </h1>
@@ -58,11 +59,7 @@ const ProgramOverview = () => {
           The course is designed to be accessible to all students, especially those who are underprivileged.
         </h1>
       </div>
-      
-  </div>
-
-
-    
+    </div>
   );
 }
 
