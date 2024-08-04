db78ddef26fdafca6ddf4621631fd83bf7f67a8c
Ben <52263490+kn4ihp@users.noreply.github.com>
2024-08-04 01:48:06 -0400

    Update page.tsx
    small screens

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 const About = () => {
   return (
     <>
-      <div className="flex-1 px-20 text-center">
-        <div className="flex-1 pt-36 padding-x hero__subtitle py-9">
-          <h1 className="hero__title justify-center text-center items-center">About Us</h1>
-          <p className="text-start">
+      <div className="flex  px-20 text-center">
+        <div className="flex-1 pt-36 padding-x hero__subtitle py-9 rounded-xl border-2 border-rose-500">
+          <h1 className="hero__title justify-center text-center items-center text-3xl md:text-6xl">About Us</h1>
+          <p className="pt-5 w-auto text-start text-sm sm:text-xl ">
             Youth in Nigeria have little exposure to computer programming.
             This lack of exposure hinders future opportunities. NaijaCoder is
             geared towards using computer science as a tool for a better economic,
@@ -22,23 +22,19 @@ const About = () => {
           </p>
         </div>
 
-        <div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen padding-x">
-          <div className="hero__image">
-            <Image src="./aboutusyouth.png" alt="hero" fill className="object-contain" />
-          </div>
-        </div>
+       
       </div>
 
       <div className="flex-col gap-5 relative z-0 w-max mx-auto">
-        <div className="text-center justify-between font-bold from-stone-950 text-6xl p-3 pt-6 pb-14 w-full my-3 object-contain">
+        <div className="text-center justify-between font-bold from-stone-950  text-3xl md:text-6xl p-3 pt-6 pb-14 w-full my-3 object-contain">
           Meet the NaijaCoder Team
         </div>
 
-        <div className="flex justify-center gap-14 p-3 sm:flex-shrink">
+        <div className="flex justify-center sm:gap-14 p-3 sm:flex-shrink">
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Daniel_Alabi.jpg" alt="Daniel Alabi" width={118} height={118} className="object-contain" />
-              <div>Daniel Alabi, Ph.D.</div>
+              <Image src="/Daniel_Alabi.jpg" alt="Daniel Alabi" width={118} height={118} className="object-contain" />
+              <div className= "text-sm sm:text-lg text-start">Daniel Alabi, Ph.D.</div>
             </div>
             <div className="name__title mb-2">President and Lead Instructor</div>
             <div>
@@ -50,7 +46,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Lekan_Afuye.jpg" alt="Lekan Afuye" width={118} height={118} className="object-contain" />
+              <Image src="/Lekan_Afuye.jpg" alt="Lekan Afuye" width={118} height={118} className="object-contain" />
               <div>Lekan Afuye, Ph.D.</div>
             </div>
             <div className="name__title">Vice President</div>
@@ -63,7 +59,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Alida_Monaco.jpg" alt="Alida Monaco" width={118} height={118} className="object-contain" />
+              <Image src="/Alida_Monaco.jpg" alt="Alida Monaco" width={118} height={118} className="object-contain" />
               <div>Alida Monaco</div>
             </div>
             <div className="name__title">Chief of Operations</div>
@@ -75,10 +71,10 @@ const About = () => {
           </div>
         </div>
 
-        <div className="flex justify-center gap-14 p-3 sm:flex-shrink">
+        <div className="flex justify-center sm:gap-14 p-3 sm:flex-shrink">
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Philip_Abel.jpg" alt="Philip Abel" width={118} height={118} className="object-contain" />
+              <Image src="/Philip_Abel.jpg" alt="Philip Abel" width={118} height={118} className="object-contain" />
               <div>Philip Abel</div>
             </div>
             <div className="name__title">Secretary</div>
@@ -91,7 +87,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Zaidat_Ibrahim.jpg" alt="Zaidat Ibrahim" width={118} height={118} className="object-contain" />
+              <Image src="/Zaidat_Ibrahim.jpg" alt="Zaidat Ibrahim" width={118} height={118} className="object-contain" />
             </div>
             <div className="name__title">Zaidat Ibrahim</div>
             <div className="linkedin__link">
@@ -103,7 +99,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Victory_Yinka-Banjo.jpg" alt="Victory Yinka-Banjo" width={118} height={118} className="object-contain" />
+              <Image src="/Victory_Yinka-Banjo.jpg" alt="Victory Yinka-Banjo" width={118} height={118} className="object-contain" />
             </div>
             <div className="name__title">Victory Yinka-Banjo</div>
             <div className="linkedin_link">
@@ -114,10 +110,10 @@ const About = () => {
           </div>
         </div>
 
-        <div className="flex justify-center gap-14 p-3 sm:flex-shrink">
+        <div className="flex justify-center sm:gap-14 p-3 sm:flex-shrink">
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Ojima_Abraham.jpg" alt="Ojima Abraham" width={118} height={118} className="object-contain" />
+              <Image src="/Ojima_Abraham.jpg" alt="Ojima Abraham" width={118} height={118} className="object-contain" />
             </div>
             <div className="name__title">Ojima Abraham</div>
             <div className="linkedin__link">
@@ -129,7 +125,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Atinuke_Adegbile.jpg" alt="Atinuke Adegbile" width={118} height={118} className="object-contain" />
+              <Image src="/Atinuke_Adegbile.jpg" alt="Atinuke Adegbile" width={118} height={118} className="object-contain" />
             </div>
             <div className="name__title">Atinuke Adegbile</div>
             <div className="linkedin__link">
@@ -141,7 +137,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Hamidah_Oderinwale.jpg" alt="Hamidah Oderinwale" width={118} height={118} className="object-contain" />
+              <Image src="/Hamidah_Oderinwale.jpg" alt="Hamidah Oderinwale" width={118} height={118} className="object-contain" />
               <div>Hamidah Oderinwale</div>
             </div>
             <div className="linkedin__link">
@@ -150,10 +146,10 @@ const About = () => {
           </div>
         </div>
 
-        <div className="flex justify-center gap-14 p-3 sm:flex-shrink">
+        <div className="flex justify-center sm:gap-14 p-3 sm:flex-shrink">
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Eric_Mibuari.jpg" alt="Eric Mibuari" width={118} height={118} className="object-contain" />
+              <Image src="/Eric_Mibuari.jpg" alt="Eric Mibuari" width={118} height={118} className="object-contain" />
               <div>Eric Mibuari, Ph.D</div>
               </div>
            
@@ -164,7 +160,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Joshua_Nwozor.jpg" alt="Joshua Nwozor" width={118} height={118} className="object-contain" />
+              <Image src="/Joshua_Nwozor.jpg" alt="Joshua Nwozor" width={118} height={118} className="object-contain" />
               <div>Joshua Nwozor</div>
             </div>
             <div className="linkedin_link">
@@ -176,7 +172,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Akachukwu_Obi.jpg" alt="Akachukwu Obi" width={118} height={118} className="object-contain" />
+              <Image src="/Akachukwu_Obi.jpg" alt="Akachukwu Obi" width={118} height={118} className="object-contain" />
               <div>Akachukwu Obi, Ph.D</div>
             </div>
           
@@ -187,10 +183,10 @@ const About = () => {
           </div>
         </div>
 
-        <div className="flex justify-center gap-14 p-3 sm:flex-shrink">
+        <div className="flex justify-center sm:gap-14 p-3 sm:flex-shrink">
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Joseph_Ekpenyong.jpg" alt="Joseph Ekpenyong" width={118} height={118} className="object-contain" />
+              <Image src="/Joseph_Ekpenyong.jpg" alt="Joseph Ekpenyong" width={118} height={118} className="object-contain" />
             </div>
             <div className="name__title">Joseph Ekpenyong</div>
             <div className="linkedin_link">
@@ -200,7 +196,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Ekene_Ezeunala.jpg" alt="Ekene Ezeunala" width={118} height={118} className="object-contain" />
+              <Image src="/Ekene_Ezeunala.jpg" alt="Ekene Ezeunala" width={118} height={118} className="object-contain" />
             </div>
             <div className="name__title">Ekene Ezeunala</div>
             <div className="linkedin__link">
@@ -211,7 +207,7 @@ const About = () => {
 
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Benem_Davids.jpg" alt="Benem Davids" width={118} height={118} className="object-contain" />
+              <Image src="/Benem_Davids.jpg" alt="Benem Davids" width={118} height={118} className="object-contain" />
               <div>Benem Davids, Ph.D.</div>
             </div>
             <div className="name__title">Webmaster</div>
@@ -225,10 +221,10 @@ const About = () => {
           Advisory Team
         </div>
 
-        <div className="flex justify-center gap-14 p-3 sm:flex-shrink">
+        <div className="flex justify-center sm:gap-14 p-3 sm:flex-shrink">
           <div className="flex flex-col items-center text-center">
             <div className="image__card mb-2">
-              <Image src="./Jelani_Nelson.jpg" alt="Jelani Nelson" width={118} height={118} className="object-contain" />
+              <Image src="/Jelani_Nelson.jpg" alt="Jelani Nelson" width={118} height={118} className="object-contain" />
               <div>Jelani Nelson, Ph.D</div>
             </div>
             <div className="name__title">Lead Advisor</div>
