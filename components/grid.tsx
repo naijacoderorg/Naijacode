53688e0d03934a8ffb502d333da0e53355785e38
Ben <52263490+kn4ihp@users.noreply.github.com>
2024-08-04 02:14:21 -0400

    Update grid.tsx

--- components/grid.tsx
+++ components/grid.tsx
@@ -93,7 +93,7 @@ export default teamMembers;
 
 
 
-const Grid = () => {
+const grid = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-[896px] w-full mx-auto">
       {teamMembers.map((member, index) => (
