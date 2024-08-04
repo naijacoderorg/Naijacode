c168b388f604318ea92dae253c7f677865bf1cbc
Ben <52263490+kn4ihp@users.noreply.github.com>
2024-08-04 02:26:28 -0400

    Update and rename grid.tsx to Gridz.tsx

--- components/Gridz.tsx
+++ components/Gridz.tsx
@@ -93,7 +93,7 @@ export default teamMembers;
 
 
 
-const grid = () => {
+const Gridz = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-[896px] w-full mx-auto">
       {teamMembers.map((member, index) => (
@@ -124,4 +124,4 @@ const grid = () => {
   );
 };
 
-export default grid;
+export default Gridz;
