269c13f65aa2fac31f1d3c94d9c4271ca4f871d7
Daniel Alabi <alabid@users.noreply.github.com>
2024-09-25 10:50:31 -0400

    Added student application

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -12,7 +12,7 @@ const applications = () => {
           <p className="text-lg mb-8">
             We are excited to offer you the opportunity to apply to our program. Click the link below to start your application.
           </p>
-          <Link href="https://docs.google.com/forms/d/e/1FAIpQLSeFyHnAmFwI7QUpAiSD3PaUY-8dPyDl_SuXG9Nt0j6SpILIUw/closedform">
+          <Link href="https://apply.naijacoder.com/">
             <a className="bg-emerald-500 text-white px-6 py-3 rounded-lg text-lg font-semibold hover:bg-emerald-600">
               Start Your Application
             </a>
